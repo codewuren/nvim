52489d2561166a17e8d6ebc1acb165cbc29a0aa6
codewuren <codewuren@gmail.com>
2024-04-23 22:41:31 +0800

    Modify cmp.lua (Change max_item_count)

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,7 +30,7 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp', max_item_count = 10 },
     -- { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
